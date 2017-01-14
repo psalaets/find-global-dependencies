5d02dd26e6c158a95186e1e80fa2c858c0aca8d7
Paul Salaets <psalaets@viimed.com>
2017-01-14 16:49:01 -0500

    ability to ignore big collections of globals by environment type

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var babylon = require('babylon');
 var traverse = require('babel-traverse').default;
+var globals = require('globals');
 
 module.exports = findGlobalDeps;
 
-function findGlobalDeps(code) {
+function findGlobalDeps(code, options = {additionalIgnoreLists: []}) {
   var globalDeps = new Set();
 
   var ast = babylon.parse(code);
@@ -15,6 +16,14 @@ function findGlobalDeps(code) {
         }
 
         var identifierName = path.node.name;
+
+        var isIgnored = options.additionalIgnoreLists.some(listName => {
+          var listObject = globals[listName];
+          return identifierName in listObject;
+        });
+
+        if (isIgnored) return;
+
         var parent = path.findParent(path => path.isBlock() || path.isFunction())
         var bindingExists = parent.scope.hasBinding(identifierName);
 
--- test/built-ins-test.js
+++ test/built-ins-test.js
@@ -14,3 +14,91 @@ test('ignores javascript built-ins', function(t) {
 
   setEquals(t, result, new Set([]));
 });
+
+test('can also ignore browser built-ins', function(t) {
+  t.plan(1);
+
+  var code = `
+    ${Object.keys(globals.builtin).join('\n')}
+    ${Object.keys(globals.browser).join('\n')}
+  `;
+  var result = find(code, {
+    additionalIgnoreLists: ['browser']
+  });
+
+  setEquals(t, result, new Set([]));
+});
+
+test('can also ignore node built-ins', function(t) {
+  t.plan(1);
+
+  var code = `
+    ${Object.keys(globals.builtin).join('\n')}
+    ${Object.keys(globals.node).join('\n')}
+  `;
+  var result = find(code, {
+    additionalIgnoreLists: ['node']
+  });
+
+  setEquals(t, result, new Set([]));
+});
+
+test('can also ignore serviceworker built-ins', function(t) {
+  t.plan(1);
+
+  var code = `
+    ${Object.keys(globals.builtin).join('\n')}
+    ${Object.keys(globals.worker).join('\n')}
+  `;
+  var result = find(code, {
+    additionalIgnoreLists: ['worker']
+  });
+
+  setEquals(t, result, new Set([]));
+});
+
+test('can also ignore commonjs built-ins', function(t) {
+  t.plan(1);
+
+  var code = `
+    ${Object.keys(globals.builtin).join('\n')}
+    ${Object.keys(globals.commonjs).join('\n')}
+  `;
+  var result = find(code, {
+    additionalIgnoreLists: ['commonjs']
+  });
+
+  setEquals(t, result, new Set([]));
+});
+
+test('can also ignore amd built-ins', function(t) {
+  t.plan(1);
+
+  var code = `
+    ${Object.keys(globals.builtin).join('\n')}
+    ${Object.keys(globals.amd).join('\n')}
+  `;
+  var result = find(code, {
+    additionalIgnoreLists: ['amd']
+  });
+
+  setEquals(t, result, new Set([]));
+});
+
+test('can ignore any combo of additional built-ins', function(t) {
+  t.plan(1);
+
+  var code = `
+    ${Object.keys(globals.builtin).join('\n')}
+    ${Object.keys(globals.browser).join('\n')}
+    ${Object.keys(globals.node).join('\n')}
+    ${Object.keys(globals.worker).join('\n')}
+    ${Object.keys(globals.commonjs).join('\n')}
+    ${Object.keys(globals.amd).join('\n')}
+  `;
+  var result = find(code, {
+    additionalIgnoreLists: 'browser node worker commonjs amd'.split(' ')
+  });
+
+  setEquals(t, result, new Set([]));
+});
